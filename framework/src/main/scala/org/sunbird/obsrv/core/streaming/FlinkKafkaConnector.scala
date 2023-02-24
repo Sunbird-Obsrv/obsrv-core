c1159002bb4dc69cbfbd3482e5dd9f2ff3dba17c
Anand Parthasarathy <anandp504@gmail.com>
2023-02-24 16:41:58 +0530

    Remove unused kafka connector code

--- framework/src/main/scala/org/sunbird/obsrv/core/streaming/FlinkKafkaConnector.scala
+++ framework/src/main/scala/org/sunbird/obsrv/core/streaming/FlinkKafkaConnector.scala
@@ -3,33 +3,11 @@ package org.sunbird.obsrv.core.streaming
 import org.apache.flink.connector.base.DeliveryGuarantee
 import org.apache.flink.connector.kafka.sink.KafkaSink
 import org.apache.flink.connector.kafka.source.KafkaSource
-import org.apache.flink.streaming.api.functions.sink.SinkFunction
-import org.apache.flink.streaming.api.functions.source.SourceFunction
-import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaProducer}
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer.Semantic
 import org.sunbird.obsrv.core.serde._
 
 import scala.collection.mutable
 
 class FlinkKafkaConnector(config: BaseJobConfig[_]) extends Serializable {
-
-  /*
-  def kafkaMapSource(kafkaTopic: String): SourceFunction[mutable.Map[String, AnyRef]] = {
-    new FlinkKafkaConsumer[mutable.Map[String, AnyRef]](kafkaTopic, new MapDeserializationSchema, config.kafkaConsumerProperties)
-  }
-
-  def kafkaMapSink(kafkaTopic: String): SinkFunction[mutable.Map[String, AnyRef]] = {
-    new FlinkKafkaProducer[mutable.Map[String, AnyRef]](kafkaTopic, new MapSerializationSchema(kafkaTopic), config.kafkaProducerProperties, Semantic.AT_LEAST_ONCE)
-  }
-
-  def kafkaStringSource(kafkaTopic: String): SourceFunction[String] = {
-    new FlinkKafkaConsumer[String](kafkaTopic, new StringDeserializationSchema, config.kafkaConsumerProperties)
-  }
-
-  def kafkaStringSink(kafkaTopic: String): SinkFunction[String] = {
-    new FlinkKafkaProducer[String](kafkaTopic, new StringSerializationSchema(kafkaTopic), config.kafkaProducerProperties, Semantic.AT_LEAST_ONCE)
-  }
-  */
 
   def kafkaStringSource(kafkaTopic: String): KafkaSource[String] = {
     KafkaSource.builder[String]()
